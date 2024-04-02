1452fb6fc44a1f98f8b4f92cb644f6213610c935
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-04-01 21:16:34 -0300

    01/04/2024
    Alterações:
    >Criado o index
    >Adicionado uma imagem parallax de fundo
    >Criado e alinhado a sessão dos jogos
    >Baixado os ícones

--- content/JAVASCRIPT/BaseScript.js
+++ content/JAVASCRIPT/BaseScript.js
@@ -1,17 +1,21 @@
-const container = document.querySelector('div#magic-grid')
 const colors = ['#e74c3c', '#8e44ad', '#3498db', '#e67e22', '#2ecc71']
-const SQUARES = 800
 
-for(let i = 0; i < SQUARES; i++) {
-    const square = document.createElement('div')
-    square.classList.add('square')
-
-    square.addEventListener('mouseover', () => setColor(square))
-
-    square.addEventListener('mouseout', () => removeColor(square))
-
-    container.appendChild(square)
-}
+const setGrid = () => {
+    const container = document.querySelector('div#magic-grid')
+    
+    const SQUARES = 800
+    
+    for(let i = 0; i < SQUARES; i++) {
+        const square = document.createElement('div')
+        square.classList.add('square')
+    
+        square.addEventListener('mouseover', () => setColor(square))
+    
+        square.addEventListener('mouseout', () => removeColor(square))
+    
+        container.appendChild(square)
+    }
+    }
 
 function setColor(element) {
    const color = getRandomColor()
@@ -28,3 +32,22 @@ function getRandomColor() {
     return colors[Math.floor(Math.random() * colors.length)]
 }
 
+//Adicionando cabeçalho//
+const body = document.body
+
+const head = document.createElement('nav')
+head.setAttribute("class", "navbar")
+
+fetch("header.html")
+.then(res => res.text())
+.then(res =>{
+    head.innerHTML = res
+    body.prepend(head)
+    setGrid()
+})
+
+
+
+//Adicionando Rodapé//
+
+//??//
