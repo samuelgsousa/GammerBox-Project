05f39599afc6a7d1cadf6257d0bd13674cc9e75f
Samuel Sousa <143044276+samuelgsousa@users.noreply.github.com>
2024-04-03 21:00:46 -0300

    03/04/2024
    Alterações;
    >Criado a parte de perfil com as avaliações e ícones
    >Ajustado para deixar responsivo

---
+++ content/JAVASCRIPT/IndexScript.js
@@ -0,0 +1,9 @@
+$(document).ready(() => {
+
+const img = ['cv-Elden-ring.svg', 'cv-Minecraft.svg', "cv-Hollow-Knight.svg", "cv-Palworld.svg", "cv-RDR2.svg", "cv-TLOU.svg" ]
+const i = Math.floor(Math.random() * img.length - 1)
+const foto = img[i]
+
+$('#cover').css("background-image", `url(../images/index-games/Cover/${foto})`)
+
+})
